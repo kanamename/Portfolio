814739ea6edf9a81f01de913281cbcf54b936236
kanamename <ebinakota.h@gmail.com>
2021-01-18 17:52:32 +0900

    Mypageのテストプログラム作成

--- tests/Feature/UsersControllerTest.php
+++ tests/Feature/UsersControllerTest.php
@@ -7,15 +7,20 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\User;
 
-class HomeControllerTest extends TestCase
+class UsersControllerTest extends TestCase
 {
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testMypage()
     {
+        // 認証エラーとなるか
+        $response = $this->get(route('mypage'));
+        $response->assertStatus(302);
+
+        // マイページへ遷移できるか
         $response = $this
             ->actingAs(User::find(1))
             ->get(route('mypage'));
